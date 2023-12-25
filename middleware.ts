d06d9d99a2260d02d538864cda3311f995f8c00b
alex <alexandrurf@proton.me>
2023-12-25 16:37:29 +0200

    another test

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  secretKey: "sk_test_MI241X3UGlGNqjwRA14MJfJA3Pm8O2usGz88fD6ojM",
 });
 
 export const config = {
